749dedbbe2ade5fb22d94a991ab45fcf662408fb
FedorIzvekov <fedor.izvekov@gmail.com>
2019-04-03 21:40:58 +0300

    statemachine: added statemachine tests

--- statemachine/src/test/java/com/fedorizvekov/statemachine/config/StateMachineConfigTest.java
+++ statemachine/src/test/java/com/fedorizvekov/statemachine/config/StateMachineConfigTest.java
@@ -1,7 +1,5 @@
 package com.fedorizvekov.statemachine.config;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.fedorizvekov.statemachine.model.enums.RegistrationEvent;
 import com.fedorizvekov.statemachine.model.enums.RegistrationState;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.test.StateMachineTestPlanBuilder;
 
 @SpringBootTest
 public class StateMachineConfigTest {
@@ -19,98 +18,282 @@ public class StateMachineConfigTest {
 
     @Test
     @DisplayName("Should start from INITIAL state")
-    void shouldStart_fromInitialState() {
+    void shouldStart_fromInitialState() throws Exception {
         var stateMachine = factory.getStateMachine();
-        assertThat(stateMachine.getState().getId()).isEqualTo(RegistrationState.INITIAL);
+
+        var testPlan = StateMachineTestPlanBuilder.<RegistrationState, RegistrationEvent>builder()
+                .stateMachine(stateMachine)
+                .step()
+                .expectStates(RegistrationState.INITIAL)
+                .expectStateChanged(0)
+                .and().build();
+
+        testPlan.test();
     }
 
 
     @Test
     @DisplayName("Should return to SELECT_IDENTIFICATION_METHOD state")
-    void shouldReturn_toSelectIdentificationMethodState() {
+    void shouldReturn_toSelectIdentificationMethodState() throws Exception {
         var stateMachine = factory.getStateMachine();
 
-        stateMachine.sendEvent(RegistrationEvent.NEW_USER_SAVED);
-        stateMachine.sendEvent(RegistrationEvent.EMAIL_CONFIRMED);
-        stateMachine.sendEvent(RegistrationEvent.PHONE_CONFIRMED);
-        stateMachine.sendEvent(RegistrationEvent.SELECT_FAST_IDENTIFICATION);
-        stateMachine.sendEvent(RegistrationEvent.CANCEL_SELECTION);
-        assertThat(stateMachine.getState().getId()).isEqualTo(RegistrationState.SELECT_IDENTIFICATION_METHOD);
+        var testPlan = StateMachineTestPlanBuilder.<RegistrationState, RegistrationEvent>builder()
+                .stateMachine(stateMachine)
+                .step()
+                .expectStates(RegistrationState.INITIAL)
+                .expectStateChanged(0)
 
-        stateMachine.sendEvent(RegistrationEvent.SELECT_FULL_IDENTIFICATION);
-        stateMachine.sendEvent(RegistrationEvent.CANCEL_SELECTION);
-        assertThat(stateMachine.getState().getId()).isEqualTo(RegistrationState.SELECT_IDENTIFICATION_METHOD);
+                .and().step()
+                .sendEvent(RegistrationEvent.NEW_USER_SAVED)
+                .expectState(RegistrationState.EMAIL_VERIFICATION)
+                .expectStateChanged(1)
 
-        stateMachine.sendEvent(RegistrationEvent.SELECT_FAST_IDENTIFICATION);
-        stateMachine.sendEvent(RegistrationEvent.SEND_DATA_REQUEST);
-        stateMachine.sendEvent(RegistrationEvent.FAILED_ANSWER);
-        assertThat(stateMachine.getState().getId()).isEqualTo(RegistrationState.SELECT_IDENTIFICATION_METHOD);
+                .and().step()
+                .sendEvent(RegistrationEvent.EMAIL_CONFIRMED)
+                .expectState(RegistrationState.PHONE_VERIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.PHONE_CONFIRMED)
+                .expectState(RegistrationState.SELECT_IDENTIFICATION_METHOD)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.SELECT_FAST_IDENTIFICATION)
+                .expectState(RegistrationState.FAST_IDENTIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.CANCEL_SELECTION)
+                .expectState(RegistrationState.SELECT_IDENTIFICATION_METHOD)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.SELECT_FULL_IDENTIFICATION)
+                .expectState(RegistrationState.FULL_IDENTIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.CANCEL_SELECTION)
+                .expectState(RegistrationState.SELECT_IDENTIFICATION_METHOD)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.SELECT_FAST_IDENTIFICATION)
+                .expectState(RegistrationState.FAST_IDENTIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.SEND_DATA_REQUEST)
+                .expectState(RegistrationState.DATA_REQUESTED)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.FAILED_ANSWER)
+                .expectState(RegistrationState.SELECT_IDENTIFICATION_METHOD)
+                .expectStateChanged(1)
+
+                .and().build();
+
+        testPlan.test();
     }
 
 
     @Test
     @DisplayName("Should return to FULL_IDENTIFICATION state")
-    void shouldReturn_toFullIdentificationState() {
+    void shouldReturn_toFullIdentificationState() throws Exception {
         var stateMachine = factory.getStateMachine();
 
-        stateMachine.sendEvent(RegistrationEvent.NEW_USER_SAVED);
-        stateMachine.sendEvent(RegistrationEvent.EMAIL_CONFIRMED);
-        stateMachine.sendEvent(RegistrationEvent.PHONE_CONFIRMED);
-        stateMachine.sendEvent(RegistrationEvent.SELECT_FULL_IDENTIFICATION);
-        stateMachine.sendEvent(RegistrationEvent.PHOTO_UPLOADED);
-        stateMachine.sendEvent(RegistrationEvent.FAILED_PHOTO_VERIFICATION);
+        var testPlan = StateMachineTestPlanBuilder.<RegistrationState, RegistrationEvent>builder()
+                .stateMachine(stateMachine)
+                .step()
+                .expectStates(RegistrationState.INITIAL)
+                .expectStateChanged(0)
 
-        assertThat(stateMachine.getState().getId()).isEqualTo(RegistrationState.FULL_IDENTIFICATION);
+                .and().step()
+                .sendEvent(RegistrationEvent.NEW_USER_SAVED)
+                .expectState(RegistrationState.EMAIL_VERIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.EMAIL_CONFIRMED)
+                .expectState(RegistrationState.PHONE_VERIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.PHONE_CONFIRMED)
+                .expectState(RegistrationState.SELECT_IDENTIFICATION_METHOD)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.SELECT_FULL_IDENTIFICATION)
+                .expectState(RegistrationState.FULL_IDENTIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.PHOTO_UPLOADED)
+                .expectState(RegistrationState.PHOTO_VERIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.FAILED_PHOTO_VERIFICATION)
+                .expectState(RegistrationState.FULL_IDENTIFICATION)
+                .expectStateChanged(1)
+
+                .and().build();
+
+        testPlan.test();
     }
 
 
     @Test
     @DisplayName("Should finish to COMPLETED state via SELECT_FAST_IDENTIFICATION event")
-    void shouldFinish_toCompletedState_viaSelectFastIdentificationEvent() {
+    void shouldFinish_toCompletedState_viaSelectFastIdentificationEvent() throws Exception {
         var stateMachine = factory.getStateMachine();
 
-        stateMachine.sendEvent(RegistrationEvent.NEW_USER_SAVED);
-        stateMachine.sendEvent(RegistrationEvent.EMAIL_CONFIRMED);
-        stateMachine.sendEvent(RegistrationEvent.PHONE_CONFIRMED);
-        stateMachine.sendEvent(RegistrationEvent.SELECT_FAST_IDENTIFICATION);
-        stateMachine.sendEvent(RegistrationEvent.SEND_DATA_REQUEST);
-        stateMachine.sendEvent(RegistrationEvent.SUCCESSFUL_ANSWER);
+        var testPlan = StateMachineTestPlanBuilder.<RegistrationState, RegistrationEvent>builder()
+                .stateMachine(stateMachine)
+                .step()
+                .expectStates(RegistrationState.INITIAL)
+                .expectStateChanged(0)
 
-        assertThat(stateMachine.getState().getId()).isEqualTo(RegistrationState.COMPLETED);
+                .and().step()
+                .sendEvent(RegistrationEvent.NEW_USER_SAVED)
+                .expectState(RegistrationState.EMAIL_VERIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.EMAIL_CONFIRMED)
+                .expectState(RegistrationState.PHONE_VERIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.PHONE_CONFIRMED)
+                .expectState(RegistrationState.SELECT_IDENTIFICATION_METHOD)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.SELECT_FAST_IDENTIFICATION)
+                .expectState(RegistrationState.FAST_IDENTIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.SEND_DATA_REQUEST)
+                .expectState(RegistrationState.DATA_REQUESTED)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.SUCCESSFUL_ANSWER)
+                .expectState(RegistrationState.COMPLETED)
+                .expectStateChanged(1)
+
+                .and().build();
+
+        testPlan.test();
     }
 
 
     @Test
     @DisplayName("Should finish to COMPLETED state via SELECT_FULL_IDENTIFICATION event")
-    void shouldFinish_toCompletedState_viaSelectFullIdentificationEvent() {
+    void shouldFinish_toCompletedState_viaSelectFullIdentificationEvent() throws Exception {
         var stateMachine = factory.getStateMachine();
 
-        stateMachine.sendEvent(RegistrationEvent.NEW_USER_SAVED);
-        stateMachine.sendEvent(RegistrationEvent.EMAIL_CONFIRMED);
-        stateMachine.sendEvent(RegistrationEvent.PHONE_CONFIRMED);
-        stateMachine.sendEvent(RegistrationEvent.SELECT_FULL_IDENTIFICATION);
-        stateMachine.sendEvent(RegistrationEvent.PHOTO_UPLOADED);
-        stateMachine.sendEvent(RegistrationEvent.PHOTO_CONFIRMED);
-        stateMachine.sendEvent(RegistrationEvent.SUCCESSFUL_CHECK);
+        var testPlan = StateMachineTestPlanBuilder.<RegistrationState, RegistrationEvent>builder()
+                .stateMachine(stateMachine)
+                .step()
+                .expectStates(RegistrationState.INITIAL)
+                .expectStateChanged(0)
 
-        assertThat(stateMachine.getState().getId()).isEqualTo(RegistrationState.COMPLETED);
+                .and().step()
+                .sendEvent(RegistrationEvent.NEW_USER_SAVED)
+                .expectState(RegistrationState.EMAIL_VERIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.EMAIL_CONFIRMED)
+                .expectState(RegistrationState.PHONE_VERIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.PHONE_CONFIRMED)
+                .expectState(RegistrationState.SELECT_IDENTIFICATION_METHOD)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.SELECT_FULL_IDENTIFICATION)
+                .expectState(RegistrationState.FULL_IDENTIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.PHOTO_UPLOADED)
+                .expectState(RegistrationState.PHOTO_VERIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.PHOTO_CONFIRMED)
+                .expectState(RegistrationState.BLACKLIST_CHECKING)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.SUCCESSFUL_CHECK)
+                .expectState(RegistrationState.COMPLETED)
+                .expectStateChanged(1)
+
+                .and().build();
+
+        testPlan.test();
     }
 
 
     @Test
     @DisplayName("Should finish to BLOCKED state")
-    void shouldFinish_toBlockedState() {
+    void shouldFinish_toBlockedState() throws Exception {
         var stateMachine = factory.getStateMachine();
 
-        stateMachine.sendEvent(RegistrationEvent.NEW_USER_SAVED);
-        stateMachine.sendEvent(RegistrationEvent.EMAIL_CONFIRMED);
-        stateMachine.sendEvent(RegistrationEvent.PHONE_CONFIRMED);
-        stateMachine.sendEvent(RegistrationEvent.SELECT_FULL_IDENTIFICATION);
-        stateMachine.sendEvent(RegistrationEvent.PHOTO_UPLOADED);
-        stateMachine.sendEvent(RegistrationEvent.PHOTO_CONFIRMED);
-        stateMachine.sendEvent(RegistrationEvent.FAILED_CHECK);
+        var testPlan = StateMachineTestPlanBuilder.<RegistrationState, RegistrationEvent>builder()
+                .stateMachine(stateMachine)
+                .step()
+                .expectStates(RegistrationState.INITIAL)
+                .expectStateChanged(0)
 
-        assertThat(stateMachine.getState().getId()).isEqualTo(RegistrationState.BLOCKED);
+                .and().step()
+                .sendEvent(RegistrationEvent.NEW_USER_SAVED)
+                .expectState(RegistrationState.EMAIL_VERIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.EMAIL_CONFIRMED)
+                .expectState(RegistrationState.PHONE_VERIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.PHONE_CONFIRMED)
+                .expectState(RegistrationState.SELECT_IDENTIFICATION_METHOD)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.SELECT_FULL_IDENTIFICATION)
+                .expectState(RegistrationState.FULL_IDENTIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.PHOTO_UPLOADED)
+                .expectState(RegistrationState.PHOTO_VERIFICATION)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.PHOTO_CONFIRMED)
+                .expectState(RegistrationState.BLACKLIST_CHECKING)
+                .expectStateChanged(1)
+
+                .and().step()
+                .sendEvent(RegistrationEvent.FAILED_CHECK)
+                .expectState(RegistrationState.BLOCKED)
+                .expectStateChanged(1)
+
+                .and().build();
+
+        testPlan.test();
     }
 
 }
